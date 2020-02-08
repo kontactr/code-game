62f213b57ff0c02b8dca109548a156b629fd8a18
kontactr <gandhiyash1381997@gmail.com>
2020-02-08 15:29:07 +0530

    progress bar added for results

--- src/store/progressStore.js
+++ src/store/progressStore.js
@@ -1,11 +1,40 @@
 import { decorate, observable } from "mobx";
+import { STATUSES, STATUSES_PERCENT } from "../Utils/constants";
 
 class ProgressStore {
   progressModalTitle = "Code Compilation";
   progressModalDisplay = false;
 
+  constructor() {
+    this.resetSteps();
+  }
+
   toggleProgressDisplay = () => {
     this.progressModalDisplay = !this.progressModalDisplay;
+  };
+
+  generateState = (
+    progressValue = STATUSES_PERCENT.START,
+    status = STATUSES.NORMAL
+  ) => {
+    return { progress: progressValue, status };
+  };
+
+  changeState = (stateName, stateStatus, stateValue) => {
+    if (this.steps[stateName]) {
+      this.steps[stateName] = this.generateState(stateValue, stateStatus);
+    }
+  };
+
+  resetSteps = () => {
+    this.steps = observable({
+      reset: this.generateState(STATUSES_PERCENT.COMPLETE, STATUSES.ACTIVE),
+      fetchTree: this.generateState(STATUSES_PERCENT.START, STATUSES.NORMAL),
+      rawTree: this.generateState(STATUSES_PERCENT.START, STATUSES.NORMAL),
+      cycleCheck: this.generateState(STATUSES_PERCENT.START, STATUSES.NORMAL),
+      scopeCheck: this.generateState(STATUSES_PERCENT.START, STATUSES.NORMAL),
+      convertScalar: this.generateState(STATUSES_PERCENT.START, STATUSES.NORMAL)
+    });
   };
 }
 
